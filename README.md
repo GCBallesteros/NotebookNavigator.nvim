ec4be4c61407dcfbb3da4557e0c1e144a6b8b137
Guillem Ballesteros <guillem@maxwellrules.com>
2023-06-08 23:20:40 +0100

    1st commit of notebook navigator!

---
+++ README.md
@@ -0,0 +1,153 @@
+# 🚢 Notebook Navigator 🚢
+
+Notebook Navigator lets you manipulate and send code cells to a REPL.
+
+A great feature that comes with VSCode on by default is the ability to define
+code cells and send them to a REPL like you would do in a Jupyter notebook but
+without the hassle of jupyter notebook files. Notebook Navigator brings you
+back that functionality and more!
+
+Notebook Navigator comes with the following functions and features:
+- Jump up/down between cells
+- Execute cell (with and without jumping to the next one)
+- Create cells before/after the current one
+- Comment whole cells
+- A [mini.ai](https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-ai.md) textobject
+  specification that you can use standalone.
+- A [Hydra](https://github.com/anuvyklack/hydra.nvim) mode to quickly manipulate and execute
+  cells.
+
+This plugin also pairs really well with tools like Jupytext that allow you to
+convert easily between `ipynb` and `py` files. For this you may want to use a
+plugin such as [jupytext.vim](GCBallesteros/jupytext.vim)  (my fork with some
+extras) or the [original repo](goerz/jupytext.vim).
+
+Here is the video everybody is waiting for!
+
+This plugin is an evolution of my previous setup which you can find
+[here](https://www.maxwellrules.com/misc/nvim_jupyter.html).
+
+## What is a code cell?
+A code cell is any code between a cell marker, usually a specially designated comment
+and the next cell marker or the end of the buffer. The first line of a buffer has an 
+implicit cell marker before it.
+
+For example here are a bunch of cells on a Python script
+```python
+print("Cell 1")
+# %%
+print("This is cell 2!")
+# %%
+print("This is the last cell!")
+```
+
+
+## Installation
+Here is my [lazy.nvim](https://github.com/folke/lazy.nvim) specification for Notebook
+Navigator.
+
+I personally like to have the moving between cell commands and cell executing functions
+available through leader keymaps but will turn to the Hydra head when many cells need to
+be executed (just by smashing `x`) or for less commonly used functionality.
+```lua
+{
+  "GBallesteros/notebook-navigator.nvim",
+  keys = {
+    { "]h", function() require("notebook-navigator").move_cell "d" end },
+    { "[h", function() require("notebook-navigator").move_cell "u" end },
+    { "<leader>X", "<cmd>lua require('notebook-navigator').execute_cell()<cr>" },
+    { "<leader>x", "<cmd>lua require('notebook-navigator').execute_and_move()<cr>" },
+  },
+  dependencies = {
+    "echasnovski/mini.comment",
+    "echasnovski/mini.ai",
+    "hkupty/iron.nvim",
+    "anuvyklack/hydra.nvim",
+  },
+  event = "VeryLazy",
+  config = function()
+    local nn = require "notebook-navigator"
+    nn.setup({ activate_hydra_keys = "<leader>h" })
+  end,
+}
+```
+
+## Detailed configuration
+Any options that are not specified when calling `setup` will take on their default values.
+This is the default config:
+```lua
+{
+  -- Code cell marker. Cells start with the marker and end either at the beginning
+  -- of the next cell or at the end of the file.
+  cell_marker = "# %%",
+  -- If not `nil` the keymap defined in the string will activate the hydra head.
+  -- If you don't want to use hydra you don't need to install it either.
+  activate_hydra_keys = nil,
+  -- If `true` a hint panel will be shown when the hydra head is active. If `false`
+  -- you get a minimalistic hint on the command line.
+  show_hydra_hint = true,
+  -- Mappings while the hydra head is active.
+  hydra_keys = {
+    comment = "c",
+    execute = "X",
+    execute_and_move = "x",
+    move_up = "k",
+    move_down = "j",
+    add_cell_before = "a",
+    add_cell_after = "b",
+  },
+}
+```
+
+## Dependencies
+The only hard dependency is right now on `iron.nvim` which provides the REPL,
+although support for others like `conjure` or `yarepl` may be added if people
+want them or are willing to send in PRs.
+
+Commenting cells of code depends on an external plugin. Either
+[comment.nvim](https://github.com/numToStr/Comment.nvim) or
+[mini.comment](https://github.com/echasnovski/mini.comment) the two most
+popular choices by quite a bit. If you want support for more PRs are welcome.
+
+Finally, 'mini.ai' is not a dependency but if you want to use the provided
+textobject specification (highly recommended) you will then need to have it
+installed.
+
+
+## Yanking/Deleting cells
+If you setup the mini.ai integration (see below) you can then do things like,
+`dah` to delete a cell, `yih` to copy just the code or `vah` to select the full
+cell in visual mode. (y)ank, (d)elete and (v)isual also work while inside the
+Hydra mode!
+
+## Mini.ai integration
+The `miniai_spec` function is also a valid mini.ai textobject specification.
+Just add it to the custom_textobjects and of you are of to the races!
+
+There are two ways to configure it.
+
+### Adding the 'mini.ai' textobject spec
+You can add the textobject specification to your 'mini.ai' config.
+
+```lua
+local nn = require "notebook-navigator"
+local ai = require "mini.ai"
+
+ai.setup(
+  {
+    custom_textobjects = {
+      h = nn.miniai_spec,
+    },
+  }
+)
+```
+
+## Current limitations
+Below is a list of the current limitations which will get fixed soon enough.
+
+- Only one cell marker is allowed globably. Only languages that support the same
+  comment style will work. The default is `# %%` which works in Python but wouldn't
+  in Lua. This is the first priority on the todo list by allowing different markers for filetypes
+  for different filetypes.
+- Pasting a yanked or deleted cell while in Hydra mode is broken. Just go out of the
+  hydra and paste it in the correct place.
---
+++ lua/notebook-navigator/init.lua
@@ -0,0 +1,322 @@
+--- *NotebookNavigator.doc* Easily navigate and work with code cells
+--- *NotebookNavigator*
+---
+--- MIT License Copyright (c) 2023 Guillem Ballesteros
+---
+--- ==============================================================================
+---
+--- Jupyter notebooks are great for prototyping and quickly iterating on an idea
+--- but the are hard to version control and track how code has been executed. Both
+--- issues are much less problematic when working with scripts. VSCode does this
+--- better. This plugin attempts to bring the functionality of Jupyter and VSCode
+--- style to neovim.
+---
+--- # What is a code cell?
+--- A code cell is any code between a cell marker, usually a specially designated comment
+--- and the next cell marker or the end of the buffer. The first line of a buffer has an
+--- implicit cell marker before it.
+---
+--- # What comes bundled?~
+--- - Jump up/down between cells
+--- - Execute cell (with and without jumping to the next one)
+--- - Create cells before/after the current one
+--- - Comment whole cells
+--- - A mini.ai textobject specification that you can use standalone.
+--- - A Hydra mode to quickly manipulate and execute cells.
+---
+--- # Setup~
+--- Just run `require("notebook-navigator").setup(opts)` as you would with most Lua
+--- nvim packages. Any options that are left unspecified will take on their default
+--- values.
+
+local M = {}
+
+-- TODO before publish
+-- Organize commenters better into another function
+-- test documentation install itself correct.y
+
+local got_iron, iron = pcall(require, "iron.core")
+local got_hydra, hydra = pcall(require, "hydra")
+
+-- Define functions used by the different commenters
+local commenters = {}
+
+commenters.comment_nvim = function(cell_object)
+  local comment = require "Comment.api"
+  local curr_pos = vim.api.nvim_win_get_cursor(0)
+  local n_lines = cell_object.to.line - cell_object.from.line + 1
+
+  vim.api.nvim_win_set_cursor(0, { cell_object.from.line, 0 })
+  comment.toggle.linewise.count(n_lines)
+  vim.api.nvim_win_set_cursor(0, curr_pos)
+end
+
+commenters.mini_comment = function(cell_object)
+  local comment = require "mini.comment"
+  comment.toggle_lines(cell_object.from.line, cell_object.to.line)
+end
+
+commenters.no_comments = function(_)
+  vim.notify "[Notebook Navigator] No supported comment plugin available"
+end
+
+local has_mini_comment, _ = pcall(require, "mini.comment")
+local has_comment_nvim, _ = pcall(require, "Comment.api")
+local commenter
+if has_mini_comment then
+  commenter = commenters["mini_comment"]
+elseif has_comment_nvim then
+  commenter = commenters["comment_nvim"]
+else
+  commenter = commenters["no_comments"]
+end
+
+--- Module config
+---
+--- Default values:
+---@eval return MiniDoc.afterlines_to_code(MiniDoc.current.eval_section)
+M.config = {
+  -- Code cell marker. Cells start with the marker and end either at the beginning
+  -- of the next cell or at the end of the file.
+  cell_marker = "# %%",
+  -- If not `nil` the keymap defined in the string will activate the hydra head
+  activate_hydra_keys = nil,
+  -- If `true` a hint panel will be shown when the hydra head is active
+  show_hydra_hint = true,
+  -- Mappings while the hydra head is active.
+  hydra_keys = {
+    comment = "c",
+    execute = "X",
+    execute_and_move = "x",
+    move_up = "k",
+    move_down = "j",
+    add_cell_before = "a",
+    add_cell_after = "b",
+  },
+}
+--minidoc_afterlines_end
+
+--- Module setup
+---
+---@param config table|nil Module config table. See |NotebookNavigator.config|.
+---
+---@usage `require('cell-navigator').setup({})` (replace `{}` with your `config` table)
+---    any config parameter which you not pass will take on its default value.
+M.setup = function(config)
+  vim.validate({ config = { config, "table", true } })
+  config = vim.tbl_deep_extend("force", M.config, config or {})
+
+  vim.validate({
+    cell_marker = { config.cell_marker, "string" },
+    activate_hydra_keys = { config.activate_hydra_keys, "string", true },
+    show_hydra_hint = { config.show_hydra_hint, "boolean" },
+    hydra_keys = { config.hydra_keys, "table" },
+  })
+
+  vim.validate({
+    ["config.hydra_keys.comment"] = { config.hydra_keys.comment, "string" },
+    ["config.hydra_keys.execute"] = { config.hydra_keys.execute, "string" },
+    ["config.hydra_keys.execute_and_move"] = { config.hydra_keys.execute_and_move, "string" },
+    ["config.hydra_keys.move_up"] = { config.hydra_keys.move_up, "string" },
+    ["config.hydra_keys.move_down"] = { config.hydra_keys.move_down, "string" },
+    ["config.hydra_keys.add_cell_before"] = { config.hydra_keys.add_cell_before, "string" },
+    ["config.hydra_keys.add_cell_after"] = { config.hydra_keys.add_cell_after, "string" },
+  })
+
+  if (not got_hydra) and (config.activate_hydra_keys ~= nil) then
+    vim.notify "[NotebookNavigator] Hydra is not available.\nHydra will not be available."
+  end
+
+  if not got_iron then
+    vim.notify "[NotebookNavigator] Iron is not available.\nMost functionality will error out."
+  end
+
+  if (config.activate_hydra_keys ~= nil) and got_hydra then
+    hydra({
+      name = "NotebookNavigator",
+      mode = { "n" },
+      config = {
+        invoke_on_body = true,
+        color = "pink",
+      },
+      body = config.activate_hydra_keys,
+      heads = {
+        {
+          config.hydra_keys.move_up,
+          function()
+            M.move_cell "u"
+          end,
+          { desc = "Move up" },
+        },
+        {
+          config.hydra_keys.move_down,
+          function()
+            M.move_cell "d"
+          end,
+          { desc = "Move down" },
+        },
+        {
+          config.hydra_keys.comment,
+          M.comment_cell,
+          { desc = "Comment" },
+        },
+        {
+          config.hydra_keys.execute,
+          M.execute_cell,
+          { desc = "Execute", nowait = true },
+        },
+        {
+          config.hydra_keys.execute_and_move,
+          M.execute_and_move,
+          { desc = "Execute & Move", nowait = true },
+        },
+        {
+          config.hydra_keys.add_cell_after,
+          M.add_cell_after,
+          { desc = "Add cell after", nowait = true },
+        },
+        {
+          config.hydra_keys.add_cell_before,
+          M.add_cell_before,
+          { desc = "Add cell after", nowait = true },
+        },
+        { "q", nil, { exit = true, nowait = true, desc = "exit" } },
+        { "<esc>", nil, { exit = true, nowait = true, desc = "exit" } },
+      },
+    })
+  end
+
+  M.config = config
+end
+
+--- Returns the boundaries of the current code cell.
+---
+---@param opts string Either "i" to select the inner lines of the cell or "a" for
+---   the outer cell.
+---
+---@return table Table with keys from/to indicating the start and end of the cell.
+---   The from/to fields themselves have a line and col field.
+M.miniai_spec = function(opts)
+  local start_line = vim.fn.search("^" .. M.config.cell_marker, "bcnW")
+
+  -- Just in case the notebook is malformed and doesnt  have a cell marker at the start.
+  if start_line == 0 then
+    start_line = 1
+  else
+    if opts == "i" then
+      start_line = start_line + 1
+    end
+  end
+
+  local end_line = vim.fn.search("^" .. M.config.cell_marker, "nW") - 1
+  if end_line == -1 then
+    end_line = vim.fn.line "$"
+  end
+
+  local last_col = math.max(vim.fn.getline(end_line):len(), 1)
+
+  local from = { line = start_line, col = 1 }
+  local to = { line = end_line, col = last_col }
+
+  return { from = from, to = to }
+end
+
+--- Move between cells
+---
+--- Move between cells and indicate wheter we are at the first or last cell via the
+--- string output.
+---
+---@param dir string Movement direction. "d" for down and "u" for up.
+---
+---@return string If movement failed return "first" or "last" if we where at the
+---   first/last cell.
+M.move_cell = function(dir)
+  local search_res
+  local result
+
+  if dir == "d" then
+    search_res = vim.fn.search("^" .. M.config.cell_marker, "W")
+    if search_res == 0 then
+      result = "last"
+    end
+  else
+    search_res = vim.fn.search("^" .. M.config.cell_marker, "bW")
+    if search_res == 0 then
+      result = "first"
+    end
+  end
+
+  return result
+end
+
+--- Execute the current cell under the cursor
+M.execute_cell = function()
+  local cell_object = M.miniai_spec "i"
+
+  -- protect ourselves against the case with no actual lines of code
+  local n_lines = cell_object.to.line - cell_object.from.line + 1
+  if n_lines < 1 then
+    return nil
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(0, cell_object.from.line - 1, cell_object.to.line, 0)
+
+  iron.send(nil, lines)
+end
+
+--- Execute the current cell under the cursor and jump to next cell. If no next cell
+--- is available it will create one like Jupyter notebooks.
+M.execute_and_move = function()
+  M.execute_cell()
+  local is_last_cell = M.move_cell "d" == "last"
+
+  -- insert a new cell to replicate the behaviour of jupyter notebooks
+  if is_last_cell then
+    vim.api.nvim_buf_set_lines(0, -1, -1, false, { M.config.cell_marker, "" })
+    -- and move to it
+    M.move_cell "d"
+  end
+end
+
+--- Comment all the contents of the cell under the cursor
+---
+--- The commenting functionality is supported by external plugins. Currently the
+---- following are supported:
+--- - mini.comment
+--- - comment.nvim
+M.comment_cell = function()
+  local cell_object = M.miniai_spec "i"
+
+  -- protect against empty cells
+  local n_lines = cell_object.to.line - cell_object.from.line + 1
+  if n_lines < 1 then
+    return nil
+  end
+  commenter(cell_object)
+end
+
+--- Create a cell on top of the current one and move to it
+M.add_cell_before = function()
+  local cell_object = M.miniai_spec "a"
+
+  -- What to do on malformed notebooks? I.e. with no upper cell marker? are they malformed?
+  -- What if we have a jupytext header? Code doesn't start at top of buffer.
+  vim.api.nvim_buf_set_lines(
+    0,
+    cell_object.from.line - 1,
+    cell_object.from.line - 1,
+    false,
+    { M.config.cell_marker, "" }
+  )
+  M.move_cell "u"
+end
+
+--- Create a cell under the current one and move to it
+M.add_cell_after = function()
+  local cell_object = M.miniai_spec "a"
+
+  vim.api.nvim_buf_set_lines(0, cell_object.to.line, cell_object.to.line, false, { M.config.cell_marker, "" })
+  M.move_cell "d"
+end
+
+return M
