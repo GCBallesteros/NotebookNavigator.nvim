b336dfd63161058604fa2c0cfaac89e64049ec81
Guillem Ballesteros <guillem@maxwellrules.com>
2023-06-16 22:50:16 +0200

    refactor: Format with stylua

--- lua/notebook-navigator/init.lua
+++ lua/notebook-navigator/init.lua
@@ -39,7 +39,7 @@ local core = require "notebook-navigator.core"
 local utils = require "notebook-navigator.utils"
 
 local cell_marker = function()
-    return utils.get_cell_marker(0, M.config.cell_markers)
+  return utils.get_cell_marker(0, M.config.cell_markers)
 end
 
 -- Export directly the core functions specialized for cell markers
@@ -52,7 +52,7 @@ end
 ---@return table Table with keys from/to indicating the start and end of the cell.
 ---   The from/to fields themselves have a line and col field.
 M.miniai_spec = function(opts)
-    return core.miniai_spec(opts, cell_marker())
+  return core.miniai_spec(opts, cell_marker())
 end
 
 --- Move between cells
@@ -65,18 +65,18 @@ end
 ---@return string If movement failed return "first" or "last" if we where at the
 ---   first/last cell.
 M.move_cell = function(dir)
-    return core.move_cell(dir, cell_marker())
+  return core.move_cell(dir, cell_marker())
 end
 
 --- Run the current cell under the cursor
 M.run_cell = function()
-    core.run_cell(cell_marker())
+  core.run_cell(cell_marker())
 end
 
 --- Run the current cell under the cursor and jump to next cell. If no next cell
 --- is available it will create one like Jupyter notebooks.
 M.run_and_move = function()
-    core.run_and_move(cell_marker())
+  core.run_and_move(cell_marker())
 end
 
 --- Comment all the contents of the cell under the cursor
@@ -86,17 +86,17 @@ end
 --- - mini.comment
 --- - comment.nvim
 M.comment_cell = function()
-    core.comment_cell(cell_marker())
+  core.comment_cell(cell_marker())
 end
 
 --- Create a cell under the current one and move to it
 M.add_cell_after = function()
-    core.add_cell_after(cell_marker())
+  core.add_cell_after(cell_marker())
 end
 
 --- Create a cell on top of the current one and move to it
 M.add_cell_before = function()
-    core.add_cell_before(cell_marker())
+  core.add_cell_before(cell_marker())
 end
 
 local hydra_hint = [[
@@ -106,64 +106,64 @@ _x_: run & move down ^^          _X_: run
 ]]
 
 local function activate_hydra(config)
-    local hydra_config = {
-        name = "NotebookNavigator",
-        mode = { "n" },
-        config = {
-            invoke_on_body = true,
-            color = "pink",
-            hint = { border = "rounded" },
-        },
-        body = config.activate_hydra_keys,
-        heads = {
-            {
-                config.hydra_keys.move_up,
-                function()
-                    M.move_cell "u"
-                end,
-                { desc = "Move up" },
-            },
-            {
-                config.hydra_keys.move_down,
-                function()
-                    M.move_cell "d"
-                end,
-                { desc = "Move down" },
-            },
-            {
-                config.hydra_keys.comment,
-                M.comment_cell,
-                { desc = "Comment" },
-            },
-            {
-                config.hydra_keys.run,
-                M.run_cell,
-                { desc = "Run", nowait = true },
-            },
-            {
-                config.hydra_keys.run_and_move,
-                M.run_and_move,
-                { desc = "Run & Move", nowait = true },
-            },
-            {
-                config.hydra_keys.add_cell_after,
-                M.add_cell_after,
-                { desc = "Add cell after", nowait = true },
-            },
-            {
-                config.hydra_keys.add_cell_before,
-                M.add_cell_before,
-                { desc = "Add cell after", nowait = true },
-            },
-            { "q",     nil, { exit = true, nowait = true, desc = "exit" } },
-            { "<esc>", nil, { exit = true, nowait = true, desc = "exit" } },
-        },
-    }
-    if config.show_hydra_hint then
-        hydra_config.hint = hydra_hint
-    end
+  local hydra_config = {
+    name = "NotebookNavigator",
+    mode = { "n" },
+    config = {
+      invoke_on_body = true,
+      color = "pink",
+      hint = { border = "rounded" },
+    },
+    body = config.activate_hydra_keys,
+    heads = {
+      {
+        config.hydra_keys.move_up,
+        function()
+          M.move_cell "u"
+        end,
+        { desc = "Move up" },
+      },
+      {
+        config.hydra_keys.move_down,
+        function()
+          M.move_cell "d"
+        end,
+        { desc = "Move down" },
+      },
+      {
+        config.hydra_keys.comment,
+        M.comment_cell,
+        { desc = "Comment" },
+      },
+      {
+        config.hydra_keys.run,
+        M.run_cell,
+        { desc = "Run", nowait = true },
+      },
+      {
+        config.hydra_keys.run_and_move,
+        M.run_and_move,
+        { desc = "Run & Move", nowait = true },
+      },
+      {
+        config.hydra_keys.add_cell_after,
+        M.add_cell_after,
+        { desc = "Add cell after", nowait = true },
+      },
+      {
+        config.hydra_keys.add_cell_before,
+        M.add_cell_before,
+        { desc = "Add cell after", nowait = true },
+      },
+      { "q", nil, { exit = true, nowait = true, desc = "exit" } },
+      { "<esc>", nil, { exit = true, nowait = true, desc = "exit" } },
+    },
+  }
+  if config.show_hydra_hint then
+    hydra_config.hint = hydra_hint
+  end
 
-    hydra(hydra_config)
+  hydra(hydra_config)
 end
 
 --- Module config
@@ -171,23 +171,23 @@ end
 --- Default values:
 ---@eval return MiniDoc.afterlines_to_code(MiniDoc.current.eval_section)
 M.config = {
-    -- Code cell marker. Cells start with the marker and end either at the beginning
-    -- of the next cell or at the end of the file.
-    cell_markers = { python = "# %%", lua = "-- %%", julia = "# %%", fennel = ";; %%" },
-    -- If not `nil` the keymap defined in the string will activate the hydra head
-    activate_hydra_keys = nil,
-    -- If `true` a hint panel will be shown when the hydra head is active
-    show_hydra_hint = true,
-    -- Mappings while the hydra head is active.
-    hydra_keys = {
-        comment = "c",
-        run = "X",
-        run_and_move = "x",
-        move_up = "k",
-        move_down = "j",
-        add_cell_before = "a",
-        add_cell_after = "b",
-    },
+  -- Code cell marker. Cells start with the marker and end either at the beginning
+  -- of the next cell or at the end of the file.
+  cell_markers = { python = "# %%", lua = "-- %%", julia = "# %%", fennel = ";; %%" },
+  -- If not `nil` the keymap defined in the string will activate the hydra head
+  activate_hydra_keys = nil,
+  -- If `true` a hint panel will be shown when the hydra head is active
+  show_hydra_hint = true,
+  -- Mappings while the hydra head is active.
+  hydra_keys = {
+    comment = "c",
+    run = "X",
+    run_and_move = "x",
+    move_up = "k",
+    move_down = "j",
+    add_cell_before = "a",
+    add_cell_after = "b",
+  },
 }
 --minidoc_afterlines_end
 
@@ -198,43 +198,43 @@ M.config = {
 ---@usage `require('cell-navigator').setup({})` (replace `{}` with your `config` table)
 ---    any config parameter which you not pass will take on its default value.
 M.setup = function(config)
-    vim.validate({ config = { config, "table", true } })
-    M.config = vim.tbl_deep_extend("force", M.config, config or {})
+  vim.validate({ config = { config, "table", true } })
+  M.config = vim.tbl_deep_extend("force", M.config, config or {})
 
+  vim.validate({
+    cell_markers = { M.config.cell_markers, "table" },
+    activate_hydra_keys = { M.config.activate_hydra_keys, "string", true },
+    show_hydra_hint = { M.config.show_hydra_hint, "boolean" },
+    hydra_keys = { M.config.hydra_keys, "table" },
+  })
+
+  vim.validate({
+    ["config.hydra_keys.comment"] = { M.config.hydra_keys.comment, "string" },
+    ["config.hydra_keys.run"] = { M.config.hydra_keys.run, "string" },
+    ["config.hydra_keys.run_and_move"] = { M.config.hydra_keys.run_and_move, "string" },
+    ["config.hydra_keys.move_up"] = { M.config.hydra_keys.move_up, "string" },
+    ["config.hydra_keys.move_down"] = { M.config.hydra_keys.move_down, "string" },
+    ["config.hydra_keys.add_cell_before"] = { M.config.hydra_keys.add_cell_before, "string" },
+    ["config.hydra_keys.add_cell_after"] = { M.config.hydra_keys.add_cell_after, "string" },
+  })
+
+  for ft, marker in pairs(M.config.cell_markers) do
     vim.validate({
-        cell_markers = { M.config.cell_markers, "table" },
-        activate_hydra_keys = { M.config.activate_hydra_keys, "string", true },
-        show_hydra_hint = { M.config.show_hydra_hint, "boolean" },
-        hydra_keys = { M.config.hydra_keys, "table" },
+      ["config.cell_markers." .. ft] = { marker, "string" },
     })
+  end
 
-    vim.validate({
-        ["config.hydra_keys.comment"] = { M.config.hydra_keys.comment, "string" },
-        ["config.hydra_keys.run"] = { M.config.hydra_keys.run, "string" },
-        ["config.hydra_keys.run_and_move"] = { M.config.hydra_keys.run_and_move, "string" },
-        ["config.hydra_keys.move_up"] = { M.config.hydra_keys.move_up, "string" },
-        ["config.hydra_keys.move_down"] = { M.config.hydra_keys.move_down, "string" },
-        ["config.hydra_keys.add_cell_before"] = { M.config.hydra_keys.add_cell_before, "string" },
-        ["config.hydra_keys.add_cell_after"] = { M.config.hydra_keys.add_cell_after, "string" },
-    })
+  if (not got_hydra) and (M.config.activate_hydra_keys ~= nil) then
+    vim.notify "[NotebookNavigator] Hydra is not available.\nHydra will not be available."
+  end
 
-    for ft, marker in pairs(M.config.cell_markers) do
-        vim.validate({
-            ["config.cell_markers." .. ft] = { marker, "string" },
-        })
-    end
+  if not got_iron then
+    vim.notify "[NotebookNavigator] Iron is not available.\nMost functionality will error out."
+  end
 
-    if (not got_hydra) and (M.config.activate_hydra_keys ~= nil) then
-        vim.notify "[NotebookNavigator] Hydra is not available.\nHydra will not be available."
-    end
-
-    if not got_iron then
-        vim.notify "[NotebookNavigator] Iron is not available.\nMost functionality will error out."
-    end
-
-    if (M.config.activate_hydra_keys ~= nil) and got_hydra then
-        activate_hydra(M.config)
-    end
+  if (M.config.activate_hydra_keys ~= nil) and got_hydra then
+    activate_hydra(M.config)
+  end
 end
 
 return M
--- lua/notebook-navigator/utils.lua
+++ lua/notebook-navigator/utils.lua
@@ -1,15 +1,15 @@
 local utils = {}
 
 utils.get_cell_marker = function(bufnr, cell_markers)
-    local ft = vim.bo[bufnr].filetype
+  local ft = vim.bo[bufnr].filetype
 
-    if ft == nil or ft == "" then
-        error "Empty filetype"
-    elseif cell_markers[ft] == nil then
-        error("There's no cell marker defined for filetype " .. ft)
-    end
+  if ft == nil or ft == "" then
+    error "Empty filetype"
+  elseif cell_markers[ft] == nil then
+    error("There's no cell marker defined for filetype " .. ft)
+  end
 
-    return cell_markers[ft]
+  return cell_markers[ft]
 end
 
 return utils
