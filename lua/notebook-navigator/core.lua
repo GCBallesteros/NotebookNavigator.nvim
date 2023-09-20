4ac3819a735cef5ea2b7994da8e7286ea8861118
Connor Robertson <cnr.robertson@gmail.com>
2023-09-20 13:35:46 -0700

    feat: Add toggleterm.nvim support
    More detailed description of things that have been done to get this working:
    - add repl file to house various repl providers
    - add toggleterm run command and add repl_provider config option
    - add repl info to init and readme

--- lua/notebook-navigator/core.lua
+++ lua/notebook-navigator/core.lua
@@ -1,5 +1,5 @@
-local _, iron = pcall(require, "iron.core")
 local commenter = require "notebook-navigator.commenters"
+local get_repl = require "notebook-navigator.repls"
 
 local M = {}
 
@@ -48,7 +48,9 @@ M.move_cell = function(dir, cell_marker)
   return result
 end
 
-M.run_cell = function(cell_marker)
+M.run_cell = function(cell_marker, repl_provider, repl_args)
+  repl_args = repl_args or nil
+  repl_provider = repl_provider or "auto"
   local cell_object = M.miniai_spec("i", cell_marker)
 
   -- protect ourselves against the case with no actual lines of code
@@ -57,13 +59,12 @@ M.run_cell = function(cell_marker)
     return nil
   end
 
-  local lines = vim.api.nvim_buf_get_lines(0, cell_object.from.line - 1, cell_object.to.line, 0)
-
-  iron.send(nil, lines)
+  local repl = get_repl(repl_provider)
+  repl(cell_object.from.line, cell_object.to.line, repl_args)
 end
 
-M.run_and_move = function(cell_marker)
-  M.run_cell(cell_marker)
+M.run_and_move = function(cell_marker, repl_provider, repl_args)
+  M.run_cell(cell_marker, repl_provider, repl_args)
   local is_last_cell = M.move_cell("d", cell_marker) == "last"
 
   -- insert a new cell to replicate the behaviour of jupyter notebooks
