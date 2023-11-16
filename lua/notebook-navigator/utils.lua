974dec05dc5a86342a134e153e8fd19699aa8ac8
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2023-11-16 15:10:07 +0100

    `%%` comments as default cell markers (#23)

--- lua/notebook-navigator/utils.lua
+++ lua/notebook-navigator/utils.lua
@@ -5,11 +5,20 @@ utils.get_cell_marker = function(bufnr, cell_markers)
 
   if ft == nil or ft == "" then
     error "Empty filetype"
-  elseif cell_markers[ft] == nil then
-    error("There's no cell marker defined for filetype " .. ft)
   end
 
-  return cell_markers[ft]
+  local user_opt_cell_marker = cell_markers[ft]
+  if user_opt_cell_marker then
+    return user_opt_cell_marker
+  end
+
+  -- use double percent markers as default for cell markers
+  -- DOCS https://jupytext.readthedocs.io/en/latest/formats-scripts.html#the-percent-format
+  if not vim.bo.commentstring then
+    error("There's no cell marker and no commentstring defined for filetype " .. ft)
+  end
+  local double_percent_cell_marker = vim.bo.commentstring:format "%%"
+  return double_percent_cell_marker
 end
 
 utils.get_valid_filetypes = function(cell_markers)
