2755fd7bc0425010bb7508d38ac697b9ade06c7c
GCBallesteros <dev+github@maxwellrules.com>
2023-12-26 16:34:54 +0100

    chore: Make linter happy about unused variables

--- lua/notebook-navigator/repls.lua
+++ lua/notebook-navigator/repls.lua
@@ -3,13 +3,15 @@ local repls = {}
 local utils = require "notebook-navigator.utils"
 
 -- iron.nvim
-repls.iron = function(start_line, end_line, repl_args)
-  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, 0)
+---@diagnostic disable-next-line: unused-local
+repls.iron = function(start_line, end_line, repl_args, _cell_marker)
+  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
   require("iron.core").send(nil, lines)
 end
 
 -- toggleterm
-repls.toggleterm = function(start_line, end_line, repl_args)
+---@diagnostic disable-next-line: unused-local
+repls.toggleterm = function(start_line, end_line, repl_args, cell_marker)
   local id = 1
   local trim_spaces = true
   if repl_args then
@@ -17,7 +19,7 @@ repls.toggleterm = function(start_line, end_line, repl_args)
     trim_spaces = (repl_args.trim_spaces == nil) or repl_args.trim_spaces
   end
   local current_window = vim.api.nvim_get_current_win()
-  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, 0)
+  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
 
   if not lines or not next(lines) then
     return
